3df1233d374f9f74ce7e7954662164d64ce524f5
Michael Rooney <m.rooney93@gmail.com>
2017-10-04 16:49:50 +0100

    Add some timing code

--- Assignment 1/fib.c
+++ Assignment 1/fib.c
@@ -28,6 +28,58 @@ int fib2(int k){
 	return f;
 }
 
+void run_fib(int k){
+	struct timeval start_time; 
+	struct timeval end_time;
+	long long time_taken = 0L;
+	printf("Using slow version\n");
+	gettimeofday(&start_time, NULL);
+	int result = fib(k);
+	gettimeofday(&end_time, NULL);
+	time_taken = (end_time.tv_sec - start_time.tv_sec) * 1000000L + (end_time.tv_usec - start_time.tv_usec);
+	printf("Slow version took %lld microseconds and calculated Fibonacci number %d as %d\n", time_taken, k, result);
+}
+
+void run_fib2(int k){
+	struct timeval start_time; 
+	struct timeval end_time;
+	long long time_taken = 0L;
+	printf("Using fast version\n");
+	gettimeofday(&start_time, NULL);
+	int result = fib2(k);
+	gettimeofday(&end_time, NULL);
+	time_taken = (end_time.tv_sec - start_time.tv_sec) * 1000000L + (end_time.tv_usec - start_time.tv_usec);
+	printf("Fast version took %lld microseconds and calculated Fibonacci number %d as %d\n", time_taken, k, result);
+}
+
+// Calculates the time taken to find fibonacci numbers 1 to n inclusive.
+// Each number is calculated 10 times and the time taken is averaged.
+// The results are written to a .csv file so they can be graphed
+void time_fib(n){
+	// To "warm" the cache first
+	fib(10);
+	struct timeval start_time; 
+	struct timeval end_time;
+	long long time_taken;
+	const int num_runs = 10;
+	FILE *fp = fopen("results.csv", "w");
+	int i;
+	for(i = 1; i <= n; i++){
+		time_taken = 0;
+		int j;
+		for(j = 0; j < num_runs; j++){
+			gettimeofday(&start_time, NULL);
+			int result = fib(i);
+			gettimeofday(&end_time, NULL);
+			time_taken += (end_time.tv_sec - start_time.tv_sec) * 1000000L + (end_time.tv_usec - start_time.tv_usec);
+		}
+		time_taken = time_taken / num_runs;
+		fprintf(fp, "%lld,", time_taken);
+		printf("%d took %lld microseconds\n", i, time_taken);
+	}
+	fclose(fp);
+}
+
 int main(int argc, char *argv[]){
 	if(argc != 2){
 		printf("Error: expected a single int passed as an argument\n");
@@ -38,20 +90,8 @@ int main(int argc, char *argv[]){
 		printf("Please pass a valid non-zero integer\n");
 		return 1;
 	}
-	struct timeval start_time; 
-	struct timeval end_time;
-	long long time_taken = 0L;
-	printf("Using slow version\n");
-	gettimeofday(&start_time, NULL);
-	int result = fib(k);
-	gettimeofday(&end_time, NULL);
-	time_taken = (end_time.tv_sec - start_time.tv_sec) * 1000000L + (end_time.tv_usec - start_time.tv_usec);
-	printf("Slow version took %lld microseconds and calculated Fibonacci number %d as %d\n", time_taken, k, result);
-	printf("Using fast version\n");
-	gettimeofday(&start_time, NULL);
-	result = fib2(k);
-	gettimeofday(&end_time, NULL);
-	time_taken = (end_time.tv_sec - start_time.tv_sec) * 1000000L + (end_time.tv_usec - start_time.tv_usec);
-	printf("Fast version took %lld microseconds and calculated Fibonacci number %d as %d\n", time_taken, k, result);
+	time_fib(k);
+	// run_fib(k);
+	// run_fib2(k);
 }
 
