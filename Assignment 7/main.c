71aa811e2b8dda7e1b81ccdfaafcb0c62c6fab00
m <m.rooney93@gmail.com>
2017-12-31 10:23:01 -0800

    Rewrite data structures

--- Assignment 7/main.c
+++ Assignment 7/main.c
@@ -2,41 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct connection_cell {
-	int number;
-	struct connection_cell *next;
-};
-
-// This stores the vertex number (id), the connected vertices' numbers,
-// and the next vertex in the overall list of vertices.
-// Note: the next vertex may not have any connection/relationship to the
-// current vertex, it is just the next vertex loaded from the file.
+// This stores the vertex number (id) and all connected vertices.
+// The connections field will points to entries in the ALL_VERTICES array.
+// The connections array will default to CONNECTIONS_ARRAY_START_SIZE elements.
+// If more space is needed realloc() will be used to grow the array.
 struct vertex_cell {
 	int number;
-	struct connection_cell *connections;
-	struct vertex_cell *next;
+	struct vertex_cell **connections;
+	int num_connections;
+	int connection_array_size;
 };
 
-// The head and tail of a list that stores all vertices
-struct vertex_cell *vertex_head = NULL;
-struct vertex_cell *vertex_tail = NULL;
+const int CONNECTIONS_ARRAY_START_SIZE = 32;
+
+// An array of all vertices.
+// Defaults to VERTEX_ARRAY_START_SIZE number of elements.
+// If more space is needed realloc() will be used to grow the array.
+struct vertex_cell *ALL_VERTICES;
+#define VERTEX_ARRAY_START_SIZE 32
+int VERTEX_ARRAY_CUR_SIZE = VERTEX_ARRAY_START_SIZE;
+int NUM_VERTICES = 0;
 
 static void print_vertex_connections(struct vertex_cell *v){
-	if(v->connections == NULL){
+	if(v->connections[0] == NULL){
 		printf("Vertex %d has no connections.\n", v->number);
 		return;
 	}
 	printf("Vertex %d is connected to: ", v->number);
-	struct connection_cell *c = v->connections;
-	while(c != NULL){
-		if(c->next == NULL){
-			printf("%d.\n", c->number);			
-		} else {
-			printf("%d, ", c->number);
-		}
-		c = c->next;
+	int i;
+	for(i = 0; i < v->num_connections; i++){
+		printf("%d, ", v->connections[i]->number);
 	}
-	return;
+	printf("\n");
+}
+
+// Checks if the element at ALL_VERTICES[number] matches the passed number,
+// and returns if so as the vertex already exists. This also checks if 
+// NUM_VERTICES > 0 otherwise inserting vertex 0 will fail as the array 
+// contains 0 initially.
+static void insert_new_vertex(int vert_num){
+	if(vert_num > VERTEX_ARRAY_CUR_SIZE){
+		// TODO: realloc
+	}
+	if(ALL_VERTICES[vert_num].number == vert_num && NUM_VERTICES > 0){ 
+		return;
+	}
+	ALL_VERTICES[vert_num].number = vert_num;
+	ALL_VERTICES[vert_num].num_connections = 0;
+	ALL_VERTICES[vert_num].connections = calloc(1, sizeof(struct vertex_cell *) * CONNECTIONS_ARRAY_START_SIZE);
+	NUM_VERTICES++;
+}
+
+// First of all create the connection vertex if it does not exist already.
+static void insert_new_connection(int vert_num, int conn_num){
+	insert_new_vertex(conn_num);
+	struct vertex_cell *v = &(ALL_VERTICES[vert_num]);
+	if(v->connection_array_size == v->num_connections){
+		// TODO: realloc();
+	}
+	v->connections[v->num_connections] = &(ALL_VERTICES[conn_num]);
+	v->num_connections++;	
 }
 
 // Takes a line from the input file, tokenises it and then creates vertices from it.
@@ -49,29 +74,10 @@ static void parse_vertex_from_line(char *line){
 		printf("Warning: line contains no data after tokenisation - ignoring it.\n");
 		return;
 	}
-	struct vertex_cell *v = malloc(sizeof(struct vertex_cell));
-	if(vertex_head == NULL){
-		vertex_head = v;
-	} else {
-		vertex_tail->next = v;
-	}
-	vertex_tail = v;
-	v->number = atoi(tok);
-	struct connection_cell *prev = NULL;
-	struct connection_cell *cur = NULL;
-	int is_first = 1;
+	int vert_num = atoi(tok);
+	insert_new_vertex(vert_num);
 	while((tok = strtok(NULL, ";\n")) != NULL){
-		if(is_first){
-			is_first = 0;
-			v->connections = malloc(sizeof(struct connection_cell));
-			v->connections->number = atoi(tok);
-			prev = v->connections;
-		} else {
-			cur = malloc(sizeof(struct connection_cell));
-			cur->number = atoi(tok);
-			prev->next = cur;
-			prev = cur;
-		}
+		insert_new_connection(vert_num, atoi(tok));
 	}
 }
 
@@ -101,13 +107,14 @@ int main(int argc, char *argv[]){
 	}
 	char *filename = argv[1];
 	printf("Filename is: %s\n", filename);
+	ALL_VERTICES = calloc(1, sizeof(struct vertex_cell) * VERTEX_ARRAY_START_SIZE);
 	if(read_vertices_from_file(filename) == 1){
 		return 1;
 	}
-	struct vertex_cell *cur = vertex_head;
-	while(cur != NULL){
-		print_vertex_connections(cur);
-		cur = cur->next;
+	int i = 0;
+	for(i = 0; i < NUM_VERTICES; i++){
+		print_vertex_connections(&(ALL_VERTICES[i]));
 	}
+	
 	return 0;
 }
